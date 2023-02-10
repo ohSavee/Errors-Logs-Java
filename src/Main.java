f9e47b14029394161b2560bc6bace905c5242783
ohSavee <luca.ghinfanti@gmail.com>
2023-02-10 22:09:58 +0100

    Added & setup Log4J

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import org.apache.log4j.Logger;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
+    final static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        int[] ar = {1, 2};
+
+        //#1 method
         try{
-            int[] ar = {1, 2};
             System.out.println(ar[2]); //creating the error
         } catch (Exception e){
             appendToFile(e);
+        }
+
+
+        //#2 method
+        Main obj = new Main();
+        try{
+            obj.divide(); //creating the error
+        } catch (ArithmeticException ex){
+            logger.error(ex);
         }
 
     }
@@ -23,5 +38,11 @@ public class Main {
         } catch (Exception ex){
             System.out.println("error");
         }
+    }
+
+    private void divide(){
+
+        int i = 10 / 0;
+
     }
 }
